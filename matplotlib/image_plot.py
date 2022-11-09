2c4e3f1889ea03b1a59155b8b7969bb6bef46792
D. Todd <dt@network.net>
2022-11-09 14:10:22 -0800

    Fixed minor comment issues and missing newline at EOF

--- matplotlib/image_plot.py
+++ matplotlib/image_plot.py
@@ -1,8 +1,8 @@
+# Basic image plotting tests
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
-import time
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
 
